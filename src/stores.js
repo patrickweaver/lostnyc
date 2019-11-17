3e58dfc2f03eed5fa8f5d50951625b05d8faf29d
Glitch (nyc-memory-map) <none>
2019-11-17 18:33:05 +0000

    Fix add place bug

--- src/stores.js
+++ src/stores.js
@@ -2,4 +2,4 @@ import { writable } from 'svelte/store';
 
 export const places = writable([]);
 export const selectedPlace = writable(null);
-export const form = writable(null);
+export const modalItem = writable(null);
