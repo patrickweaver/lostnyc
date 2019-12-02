41df2990c2d8226fd71fc692e302272593735947
Glitch (lostnyc-dev) <none>
2019-12-02 23:26:41 +0000

    Move branding to env

--- config.js
+++ config.js
@@ -4,5 +4,12 @@ module.exports = {
   TILE_LAYER: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
   DEFAULT_CITY: "New York",
   SHOW_FLAGGED_PLACES: false,
-  SHOW_NO_MEMORY_PLACES: true
+  SHOW_NO_MEMORY_PLACES: true,
+  SUBHEADER_TEXT: process.env.SUBHEADER_TEXT,
+  SUBHEADER_HREF: process.env.SUBHEADER_HREF,
+  SUBHEADER_LINK_TEXT: process.env.SUBHEADER_LINK_TEXT,
+  BRAND_TEXT_COLOR: process.env.BRAND_TEXT_COLOR,
+  BRAND_CREATE_BUTTON: process.env.BRAND_CREATE_BUTTON,
+  BRAND_CREATE_BUTTON_HOVER: process.env.BRAND_CREATE_BUTTON_HOVER,
+  BRAND_HEADER_TEXT_COLOR: process.env.BRAND_HEADER_TEXT_COLOR
 }
